bb6b025ea5adb7facecd63f7082fe22f16c81e9b
sarah miller <s.k.miller422@gmail.com>
2023-03-12 22:26:13 -0400

    edited out un-needed comments

--- script.js
+++ script.js
@@ -1,68 +1,83 @@
-// // Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// // this creates teh function generatePassword
-// function generatePassword() {
-//   // this 
-//  var length = prompt("enter the length of your password (between 8 and 128 characters):")
-
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-// var generateBtn = document.querySelector("#generate");
-
-// // Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// 1. Define the characters the password can include- lowercase and uppercase letters, digits, and special characters.
-
-// 2. Prompt the user for the desired length of the password. - donzooooo
-
-// 3. Generate a random password of the desired length by randomly selecting characters from the set of characters defined in step 1.
-
-// 4. Display the generated password to the user.
-
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-// Assignment Code
+// Assignment code here - its on here 2x's ???????????
 var generateBtn = document.querySelector("#generate");
-var password = generatePassword();
-
 // Write password to the #password input
 function writePassword() {
-    
-    var passwordText = document.querySelector("#password");
-    const characterslowerCase = "abcdefghijklmnopqrstuvwxyz"
-    const charactersupperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    const charactersnumbers = "0123456789"
-    const charactersspecialCharaacters = "!@#$%^&*()"
-
-    let passwordLength = prompt("choose how long your password will be");
-    if (passwordLength >= 8  && passwordLength <= 128) {
-        alert("continue");
-    } else {
-        alert("pick between 8 to 128 characters");
-    }
-
-    let password =  generatePassword(passwordLength);
-    console.log(password);
-
-    passwordText.value = password;
-
- }
-
-// Add event listener to generate button
+  // CALLING GENERATEPASSWORD FUNCTION- HAVE TO MAKE THAT AND DEFINE THAT : STORE VALUE FROM THAT FUNCTION IN PASSWORD VARIABLE
+  var password = generatePassword();
+  // CARD WHERE GENERATED PASSWORD DISPLAYS ON SCREEN
+  var passwordText = document.querySelector("#password");
+  // DISPLAYS THAT GENERATED PASSWORD ON SCREEN
+  passwordText.value = password;
+}
+// Add event listener to generate button- WHEN USER 'CLICKS' GENERATE BUTTON - CALLS THE WRITEPASSWORD FUNCTION
 generateBtn.addEventListener("click", writePassword);
+
+// connecting html element #generate and asigning it to this generateButton
+var generateBtn = document.querySelector("#generate");
+
+var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz"
+var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numberChar = "0123456789"
+var specialChar = "~!@#$%^&*()-_=+[{]}|,<.>?"
+
+
+// here 
+function generatePassword() {
+  var validChars = []
+  var newPassword = []
+  var passwordLength = parseInt(prompt("choose a number between 8 and 128"));
+  if (isNaN(passwordLength)) {
+    alert("Must choose a number");
+    return null;
+  }
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please enter a valid number");
+    return null;
+  }
+
+  // without these here they dont pop up in the box, SETS has_ variables
+  var hasLower = confirm("Would you like lowercase letters in your password")
+  var hasUpper = confirm("Would you like uppercase letters in your password")
+  var hasNum = confirm("Would you Numbers in your password")
+  var hasSpecChar = confirm("Would you like special sharacters in your")
+
+
+  //  userChoics-  object storing user's selected password length and character type choices.
+  // 4 boolian vars. and confirm method when user clicks yes var is true, otherwise set to false
+  // will pull these into the generatepassword function to detwermin the char types chosen to include in the password 
+  var userChoices = {
+    passwordLength: passwordLength,
+    hasLower: hasLower,
+    hasUpper: hasUpper,
+    hasNum: hasNum,
+    hasSpecChar: hasSpecChar
+  }
+
+  if (userChoices.hasLower == true) {
+
+
+    validChars = validChars.concat(lowerCaseChar);
+   
+  if (userChoices.hasUpper == true) {
+    validChars = validChars.concat(upperCaseChar);
+  }
+  if (userChoices.hasSpecChar) {
+    validChars = validChars.concat(specialChar);
+  }
+  if (userChoices.hasNum) {
+    validChars = validChars.concat(numberChar);
+  }
+  console.log(validChars.join(""));
+var mashedValidChars = validChars.join("")
+  for (let index = 0; index < userChoices.passwordLength; index++) {
+    
+    
+    var x = Math.floor(Math.random() * mashedValidChars.length)
+    newPassword.push(mashedValidChars[x]);
+    console.log(newPassword)
+  }
+  return newPassword.join("")
+
+}
+}
+
